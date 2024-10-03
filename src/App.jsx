145070fe3d3b11095efc732cd1221ff584b174d1
CuongNM28123 <noobsdestroyer123@gmail.com>
2024-10-03 14:26:14 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import OrchidDetail from "./components/OrchidDetail";
+import OrchidsMenu from "./components/OrchidsMenu";
+import NavBar from "./components/NavBar";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import OriginalOrchids from "./components/OriginalOrchids";
+
+function App() {
+	return (
+		<div className="App">
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<OrchidsMenu />} />
+				<Route path="/OrchidDetail/:id" element={<OrchidDetail />} />
+				<Route path="/About" element={<About />} />
+				<Route path="/Original" element={<OriginalOrchids />} />
+				<Route path="/Contact" element={<Contact />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;

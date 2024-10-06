3aaed3f5d8d808e20035df52b04330461471d067
minhthuan098 <dinhthuan0511@gmail.com>
2024-10-06 12:34:03 +0700

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ import NavBar from "./components/NavBar";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import OriginalOrchids from "./components/OriginalOrchids";
+import Dashboard from "./dashboard/dashboard";
 
 function App() {
 	return (
 		<div className="App">
-			<NavBar />
+			{/* <NavBar /> */}
+
 			<Routes>
 				<Route path="/" element={<OrchidsMenu />} />
 				<Route path="/OrchidDetail/:id" element={<OrchidDetail />} />
 				<Route path="/About" element={<About />} />
 				<Route path="/Original" element={<OriginalOrchids />} />
 				<Route path="/Contact" element={<Contact />} />
+                <Route path="/dashboard" element={<Dashboard />} />				
 			</Routes>
 		</div>
 	);

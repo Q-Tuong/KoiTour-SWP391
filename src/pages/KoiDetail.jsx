5044c8f20ff5602cda9594159e35c6a72c049cf9
CuongNM28123 <noobsdestroyer123@gmail.com>
2024-10-06 16:38:21 +0700

    Add an add koi, update koi button

--- src/pages/KoiDetail.jsx
+++ src/pages/KoiDetail.jsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { Button, Card, Container, ListGroup } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Card, Col, Container, Image, ListGroup, Row } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import { KoiList } from "../data/KoiList";
 import NavBar from "../components/NavBar";
+import UpdateKoi from "../components/UpdateKoi";
 
 function KoiDetail() {
+	const [isOpen, setIsOpen] = useState(false);
+
 	const findId = useParams();
 	const koi = KoiList.find((obj) => {
 		return obj.koiId == findId.id;
@@ -14,9 +17,10 @@ function KoiDetail() {
 		<>
 			<NavBar />
 			<Container style={{ alignItems: "center" }}>
-				<Card border="dark" style={{ textAlign: "center" }}>
+				<Row as={"h2"}>Koi Detail</Row>
+				<Card border="dark" className="col-sm-10 offset-sm-1" style={{ textAlign: "center" }}>
 					<Card.Header as={"h2"}>{koi.productCategory}</Card.Header>
-					<Card.Body>
+					{/* <Card.Body>
 						<Card.Img src={`../${koi.imageUrl}`} alt={koi.productCategory + "img"} />
 					</Card.Body>
 					<ListGroup variant="flush">
@@ -26,11 +30,29 @@ function KoiDetail() {
 							Weight: {koi.weight}, Size: {koi.size}{" "}
 						</ListGroup.Item>
 						<ListGroup.Item>Origin: {koi.origin}</ListGroup.Item>
-					</ListGroup>
-					<Link to={"#"}>Farm that have this koi</Link>
-					<Link to={"#"}>Farm that have this koi</Link>
-					<Link to={"#"}>Farm that have this koi</Link>
+					</ListGroup> */}
+					<Row className="g-0">
+						<Col className="md-4">
+							<Image src={`../${koi.imageUrl}`} fluid alt={koi.productCategory + "img"} />
+						</Col>
+						<Col className="md-8">
+							<Card.Body>
+								<Card.Text>{koi.description}</Card.Text>
+								<ListGroup variant="flush">
+									<ListGroup.Item>Color: {koi.color}</ListGroup.Item>
+									<ListGroup.Item>Weight: {koi.weight}</ListGroup.Item>
+									<ListGroup.Item>Size: {koi.size}</ListGroup.Item>
+									<ListGroup.Item>Origin: {koi.origin}</ListGroup.Item>
+								</ListGroup>
+							</Card.Body>
+						</Col>
+					</Row>
+					<Card.Footer>
+						<Link to={"#"}>Farm that have this koi</Link>
+						<Button onClick={() => setIsOpen(true)}>Update</Button>
+					</Card.Footer>
 				</Card>
+				{isOpen && <UpdateKoi setIsOpen={setIsOpen} open={isOpen} koi={koi} />}
 			</Container>
 		</>
 	);

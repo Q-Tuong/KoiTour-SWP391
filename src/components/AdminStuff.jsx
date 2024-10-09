e61969f675147aa0536b040cd945d656b0c28915
tdat-le <tandatlxag1602@gmail.com>
2024-10-09 22:17:11 +0700

    + create account

--- src/components/AdminStuff.jsx
+++ src/components/AdminStuff.jsx
@@ -4,8 +4,11 @@ import { Table } from "antd";
 import { toast } from "react-toastify";
 
 function AdminStuff() {
-  const [user, setUser] = useState([]);
   const api = "https://66fa57e7afc569e13a9b55ec.mockapi.io/User"; //api link
+  const [createNewUserModel, setCreateNewUserModel] = useState(false);
+  const [user, setUser] = useState([]);
+  const [form] = useForm();
+  const [submitting, setSubmitting] = useState(false); // dùng để khóa nút OK khi đang submit tránh người dùng bấm OK nhiều lần
 
   const fetchUser = async () => {
     try {
@@ -81,7 +84,28 @@ function AdminStuff() {
     },
   ];
 
-  const handleCreateAccount = async () => {};
+  const handleOpenModal = () => {
+    setCreateNewUserModel(true);
+  };
+
+  const handleOffModal = () => {
+    setCreateNewUserModel(false);
+  };
+
+  const handleSubmitCreateAccount = async (user) => {
+    try {
+      setSubmitting(true);
+      const response = await axios.post(api, user);
+      toast.success("Create new account successfully");
+      setCreateNewUserModel(false);
+      form.resetFields();
+      fetchUser();
+    } catch (ex) {
+      toast.error("Create new account failed");
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   const handleDeleteUser = async (id) => {
     try {
@@ -96,10 +120,83 @@ function AdminStuff() {
   return (
     <div>
       <h1>welcome admin: </h1>
-      <Button type="primary" onClick={handleCreateAccount}>
+
+      <Button type="primary" onClick={handleOpenModal}>
         Create new account
       </Button>
+
       <Table dataSource={user} columns={columns}></Table>
+
+      <Modal
+        confirmLoading={submitting}
+        title="Create new account"
+        open={createNewUserModel}
+        onCancel={handleOffModal}
+        onOk={() => form.submit()}
+      >
+        <Form onFinish={handleSubmitCreateAccount} form={form}>
+          <Form.Item
+            label="User's Name"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please enter User's Name",
+              },
+              {
+                pattern: "^[a-zA-Z]+$",
+                message: "only word in user's name",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <FormItem
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your email",
+              },
+              {
+                pattern: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$",
+                message: "Invalid email",
+              },
+            ]}
+          >
+            <Input />
+          </FormItem>
+          <FormItem
+            label="Address"
+            name="address"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your address",
+              },
+            ]}
+          >
+            <Input />
+          </FormItem>
+          <FormItem
+            label="Phone"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                message: "Please enter your phone number",
+              },
+              {
+                pattern: "^[0-9]{9,11}$",
+                message: "Invalid phone number",
+              },
+            ]}
+          >
+            <Input />
+          </FormItem>
+        </Form>
+      </Modal>
     </div>
   );
 }
